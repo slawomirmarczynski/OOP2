f5accfde8ea834d00a50ab5f638f0dd807accea0
Sławomir Marczyński <61501424+slawomirmarczynski@users.noreply.github.com>
2024-04-16 13:00:49 +0200

    Generyki w Factory - trochę lepiej

--- README.md
+++ README.md
@@ -12,11 +12,17 @@ poligon do nauki dobrych praktyk.
 
 ## Co już jest zrobione?
 
+1. Dynamiczne ładowanie klas jest już zaimplementowane, choć bez mechanizmów
+   zabezpieczających.
+
+1. Dodany jest przykład jak pozyskać ścieżki do katalogu roboczego, katalogu
+   użytkownika itp. 
+
 1. Dodany jest przykład dynamicznego ładowania klas i wywoływania ich metod.
    Posłuży on do opracowania takiego sposobu ładowania sensorów i odbiorców
    danych, aby uczynić z nich łatwo konfigurowalne pluginy.
 
-2. Uprościliśmy znacznie kod źródłowy poprzez rezygnację z odrębnych klas
+1. Uprościliśmy znacznie kod źródłowy poprzez rezygnację z odrębnych klas
    Sensor i Sink. Połączyliśmy je w jedną klasę Component. Ale okazało się to za
    dużym uproszczeniem. Dlatego wprowadziliśmy więcej klas i klasy abstrakcyjne.
    Mamy abstrakcyjne klasy Device, Sensor i Receiver.
@@ -27,13 +33,13 @@ poligon do nauki dobrych praktyk.
      parametrze fizycznym, takim jak ciśnienie, temperatura, przyspieszenie.
    - Receiver jest czymś, co jest odbiorcą-konsumentem danych.
 
-3. Mamy sposób na powiązanie sensorów z komponentami będącymi odbiorcami danych:
+1. Mamy sposób na powiązanie sensorów z komponentami będącymi odbiorcami danych:
    stosujemy wzorzec obserwator.
 
-4. Dzięki użyciu fabryki obiektów (klasa Factory) oddzieliliśmy tworzenie
+1. Dzięki użyciu fabryki obiektów (klasa Factory) oddzieliliśmy tworzenie
    obiektów od samych obiektów.
 
-5. Mamy skuteczny sposób na czytanie konfiguracji z pliku tekstowego w formacie
+1. Mamy skuteczny sposób na czytanie konfiguracji z pliku tekstowego w formacie
    JSON. Format JSON wydaje się nieco łatwiejszy do zrozumienia i użycia niż XML,
    TOML czy YAML. YAML jest co prawda równie czytelny jak JSON, ale zadecydowała
    łatwa dostępność i prostota użycia biblioteki GSON (do czytania plików)
--- src/example/sensors/Factory.java
+++ src/example/sensors/Factory.java
@@ -94,6 +94,73 @@ public class Factory {
     }
 
     /**
+     * Dynamiczne tworzenie komponentów.
+     *
+     * @param classToCreate klasa obiektów (np. MyFoo.class).
+     * @param name          nazwa obiektu, może być wywnioskowywana z options,
+     *                      ale chcemy aby było widać że jest używana do tworzenia
+     *                      obiektów i mieć ewentualnie mieć co do niej więcej kontroli.
+     * @param type          typ obiektu, może (a może nie?) być wywnioskowana z options,
+     *                      ale chcemy podać ją tu jawnie.
+     * @param options       opcje różne; w options może być i nazwa, i typ zapisane,
+     *                      ale to nie jest używane przez createPluginComponent(),
+     *                      choć konstruktor (ładowany dynamicznie) dostaje całe
+     *                      options, więc coś może z tym zrobić.
+     * @param <T>           klasa, która jest tą klasą, której obiekty są tworzone,
+     *                      będzie to (sub)klasa klasy określonej przez classToCreate.
+     * @return utworzony obiekt klasy generycznej T, czyli jakiejś klasy.
+     */
+    private static <T> T createPluginComponent(Class<?> classToCreate, String name, String type, Object options) {
+        //@todo: zabezpieczenie przed iniekcją złośliwego lub niekompatybilnego kodu.
+        String packagePrefix = "example.sensors.";
+        List<URL> classLoaderURLs = new ArrayList<>();
+        try {
+            List<File> directories = new ArrayList<>();
+            directories.add(getWorkingDirectory());
+            directories.add(getProgramExecutableDirectory());
+            directories.remove(null);
+            int n = directories.size();
+            for (int i = 0; i < n; i++) {
+                File pluginSubdirectory = getPluginSubdirectory(directories.get(i));
+                directories.add(pluginSubdirectory);
+            }
+            directories.remove(null);
+
+            for (File directory : directories) {
+                File[] jarFiles = directory.listFiles((dir, fileName) ->
+                        fileName.endsWith(".jar") | fileName.endsWith(".class"));
+                if (jarFiles != null) {
+                    for (File jarFile : jarFiles) {
+                        classLoaderURLs.add(jarFile.toURI().toURL());
+                    }
+                }
+            }
+        } catch (URISyntaxException | MalformedURLException exception) {
+            throw new RuntimeException("folder z plugin'ami nie został odnaleziony");
+        }
+
+        try (URLClassLoader classLoader = new URLClassLoader(classLoaderURLs.toArray(new URL[0]))) {
+            String pluginClassName = packagePrefix + type;
+            Class<?> loadedClass = classLoader.loadClass(pluginClassName);
+            if (classToCreate.isAssignableFrom(loadedClass)) {
+                // Kompilator widzi tylko surowe rzutowanie typów i dlatego
+                // generuje ostrzeżenie “unchecked cast”. Nie jest w stanie
+                // zrozumieć, że rzutowanie jest chronione przez poprzednie
+                // sprawdzenie przez isAssignableFrom().
+                @SuppressWarnings("unchecked")
+                Class<T> pluginClass = (Class<T>) loadedClass;
+                Constructor<T> constructor = pluginClass.getConstructor(String.class, Object.class);
+                return constructor.newInstance(name, options);
+            } else {
+                throw new ClassCastException("Loaded class " + pluginClassName + " cannot be cast to Class<T>");
+            }
+        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException |
+                 IllegalAccessException | IOException exception) {
+            throw new RuntimeException("nie udało się stworzyć żądanego obiektu");
+        }
+    }
+
+    /**
      * Metoda tworząca listę obiektów klasy Device na podstawie konfiguracji.
      * <p>
      * Są tworzone obiekty Device, przy ich tworzeniu powstają obiekty Sensor.
@@ -181,7 +248,7 @@ public class Factory {
         var optionsAsMap = (Map<String, ?>) options;
         String name = optionsAsMap.get("name").toString();
         String type = optionsAsMap.get("type").toString();
-        return createPluginComponent(name, type, options);
+        return createPluginComponent(Device.class, name, type, options);
     }
 
     /**
@@ -192,57 +259,15 @@ public class Factory {
      * @return utworzony obiekt klasy Receiver.
      */
     private Receiver createReceiver(Object options) {
+        // Naruszamy nieco zasadę DRY, bo podobny fragment kodu jest też
+        // w metodzie createDevice. Dlaczego to robimy? Bo nie chcemy aby
+        // metoda createPluginComponent musiała wiedzieć cokolwiek na temat
+        // tego czym są options. Czyli options ma być dla niej "jakimś obiektem"
+        // bez wchodzenia w szczegóły co w nim jest. Patrz też wzorzec memento.
+        //
         var optionsAsMap = (Map<String, ?>) options;
         String name = optionsAsMap.get("name").toString();
         String type = optionsAsMap.get("type").toString();
-        return createPluginComponent(name, type, options);
-    }
-
-    /**
-     * Tworzenie obiektów takich, jakie są dostępne w dynamicznie ładowanych
-     * pluginach. Jaki obiekt tworzymy jest określone parametrem options.
-     *
-     * @param options opcje, muszą być słownikiem Map<String, ?> takim,
-     *                aby można było uzyskać nazwę i typ komponentu przez
-     *                odpowiednio get("name") i get("type").
-     * @return utworzony obiekt jakiegoś typu, jaki to typ decyduje się-samo.
-     */
-    private static <T> T createPluginComponent(String name, String type, Object options) {
-        String packagePrefix = "example.sensors.";
-        List<URL> classLoaderURLs = new ArrayList<>();
-        try {
-            List<File> directories = new ArrayList<>();
-            directories.add(getWorkingDirectory());
-            directories.add(getProgramExecutableDirectory());
-            directories.remove(null);
-            int n = directories.size();
-            for (int i = 0; i < n; i++) {
-                File pluginSubdirectory = getPluginSubdirectory(directories.get(i));
-                directories.add(pluginSubdirectory);
-            }
-            directories.remove(null);
-
-            for (File directory : directories) {
-                File[] jarFiles = directory.listFiles((dir, fileName) ->
-                        fileName.endsWith(".jar") | fileName.endsWith(".class"));
-                if (jarFiles != null) {
-                    for (File jarFile : jarFiles) {
-                        classLoaderURLs.add(jarFile.toURI().toURL());
-                    }
-                }
-            }
-        } catch (URISyntaxException | MalformedURLException exception) {
-            throw new RuntimeException("folder z plugin'ami nie został odnaleziony");
-        }
-
-        try (URLClassLoader classLoader = new URLClassLoader(classLoaderURLs.toArray(new URL[0]))) {
-            String pluginClassName = packagePrefix + type;
-            Class<? extends T> pluginClass = (Class<? extends T>) classLoader.loadClass(pluginClassName);
-            Constructor<? extends T> constructor = pluginClass.getConstructor(String.class, Object.class);
-            return constructor.newInstance(name, options);
-        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException |
-                 IllegalAccessException | IOException exception) {
-            throw new RuntimeException("nie udało się stworzyć żądanego obiektu");
-        }
+        return createPluginComponent(Receiver.class, name, type, options);
     }
 }
